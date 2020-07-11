6f27907168c96ae56d1fb2097f36159025284e01
vhamilton08 <vhamilton08@gmail.com>
2020-07-10 20:44:23 -0500

    afternoon work

--- higher-order.js
+++ higher-order.js
@@ -21,8 +21,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
-
+let evenNumbers = mixedNumbers.filter(function(element, index, array){
+return element % 2 === 0
+})
 
 
 ////////// PROBLEM 2 //////////
@@ -44,7 +45,10 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(function(element, index, array){
+  return element * 1.07
+
+})
 
 
 
@@ -63,7 +67,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce(function(acc, curr){
+ return acc + curr
+})
 
 
 
@@ -89,7 +95,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(function(element, index, array){
+  return element.CP > 200
+})
 
 
 
@@ -106,7 +114,9 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map(function(element, index, array){
+  return element.price * (1 + element.tax)
+})
 
 
 
@@ -126,6 +136,8 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.filter(element => element.owner === "Bob").map(element => element.price).reduce
+((acc, curr) => acc +curr)    
+
 
 
